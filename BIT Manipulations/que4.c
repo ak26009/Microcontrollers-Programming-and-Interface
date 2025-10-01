37bb50b1408d2dc187213c4b78289073672b37cf
Atharva Kadam <154217374+ak26009@users.noreply.github.com>
2025-10-01 08:28:50 +0530

    Update que4.c

--- BIT Manipulations/que4.c
+++ BIT Manipulations/que4.c
@@ -1,1 +1,23 @@
+// test and clear a bit
+#include<stdio.h>
 
+int main(){
+    int num,n;
+    
+    printf("Enter the num ");
+    scanf("%d",&num);
+    
+    printf("Enter the bit u want to test ");
+    scanf("%d",&n);
+    
+    int mask = 1 << n;
+    
+    if(num & mask){
+        printf("The %d bit is 1\n",n);
+        num = num & ~mask;   
+    }else{
+        printf("The %d bit is already 0\n",n);
+    }
+    
+    printf("The new number is %d ",num);
+}
